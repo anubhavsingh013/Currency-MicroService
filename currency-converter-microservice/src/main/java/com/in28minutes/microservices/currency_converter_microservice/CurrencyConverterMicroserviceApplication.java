79f7403d2d39b1e5be87c11378a2c1ed6cd750d0
anubhavsingh013 <anubhsin@amdocs.com>
2024-08-28 17:22:16 +0530

    microservices communication using feignclient

--- currency-converter-microservice/src/main/java/com/in28minutes/microservices/currency_converter_microservice/CurrencyConverterMicroserviceApplication.java
+++ currency-converter-microservice/src/main/java/com/in28minutes/microservices/currency_converter_microservice/CurrencyConverterMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.in28minutes.microservices.currency_converter_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CurrencyConverterMicroserviceApplication {
 
 	public static void main(String[] args) {
--- currency-exchange-service/src/main/java/com/example/microservices/currency_exchange_service/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/example/microservices/currency_exchange_service/CurrencyExchangeController.java
@@ -1,7 +1,5 @@
 package com.example.microservices.currency_exchange_service;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +13,7 @@ public class CurrencyExchangeController {
     @Autowired
     private Environment environment;
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
-    public CurrencyExchange getExchangeValue(@PathVariable String from , @PathVariable String to){
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from , @PathVariable String to){
         // CurrencyExchange currencyExchange= new CurrencyExchange(1000L,from,to,BigDecimal.valueOf(50));
         CurrencyExchange currencyExchange=currencyExchangeRepository.findByFromAndTo(from, to);
         if(currencyExchange==null)
